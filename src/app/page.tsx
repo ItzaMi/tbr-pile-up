ce64f92d82ecb5d9201b9c57a640a42a2c9503a1
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-12-06 18:37:54 +0000

    ✨[overall] more random factoids 💃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,20 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  const pageThickness = 0.0025 / 1000;
+  const pageThickness = 0.1 / 1000;
 
   const [file, setFile] = useState<null | File>(null);
   const [totalThickness, setTotalThickness] = useState(0);
   const [totalNumberOfPages, setTotalNumberOfPages] = useState(0);
+  const [bookWithMostPages, setBookWithMostPages] = useState<{
+    title: string;
+    pages: string;
+  } | null>(null);
+  const [bookWithLeastPages, setBookWithLeastPages] = useState<{
+    title: string;
+    pages: string;
+  } | null>(null);
+  const [numberOfBooksPerMonth, setNumberOfBooksPerMonth] = useState(20);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -34,9 +43,36 @@ export default function Home() {
             (row: any) => row["exclusive shelf"] === "to-read"
           );
           const extractedData = filteredData.map((row: any) => ({
+            Title: row["title"],
             "Number of pages": row["number of pages"],
             "Exclusive Shelf": row["exclusive shelf"],
           }));
+
+          const bookWithMostPages = extractedData.reduce((prev, current) =>
+            Number(prev["Number of pages"]) > Number(current["Number of pages"])
+              ? prev
+              : current
+          );
+          setBookWithMostPages({
+            title: bookWithMostPages["Title"],
+            pages: bookWithMostPages["Number of pages"],
+          });
+
+          const bookWithLeastPages = extractedData.reduce((prev, current) => {
+            if (prev["Number of pages"] === "") {
+              return current;
+            } else {
+              return Number(prev["Number of pages"]) <
+                Number(current["Number of pages"])
+                ? prev
+                : current;
+            }
+          });
+
+          setBookWithLeastPages({
+            title: bookWithLeastPages["Title"],
+            pages: bookWithLeastPages["Number of pages"],
+          });
 
           const totalNumberOfPages = extractedData.reduce(
             (sum, row) => sum + Number(row["Number of pages"]),
@@ -45,12 +81,16 @@ export default function Home() {
           setTotalNumberOfPages(totalNumberOfPages);
 
           const totalThickness = totalNumberOfPages * pageThickness;
-          setTotalThickness(totalThickness);
+          setTotalThickness(Number(Number(totalThickness).toFixed(2)));
         },
       });
     };
 
     reader.readAsText(file);
+  };
+
+  const handleChangeOfReadBooks = (e: ChangeEvent<HTMLInputElement>) => {
+    setNumberOfBooksPerMonth(Number(e.target.value));
   };
 
   return (
@@ -87,6 +127,43 @@ export default function Home() {
           <p className="text-xl">
             Your TBR has a total of <strong>{totalNumberOfPages} pages!</strong>
           </p>
+          <div className="h-px bg-zinc-500 w-full my-2" />
+          <div>
+            <p className="text-gray-500">Here are some other fun facts!</p>
+            <ul className="flex flex-col gap-1">
+              <li>
+                📚 That&apos;s <strong>{totalThickness} meters</strong> of
+                books!
+              </li>
+              <li>
+                📚 The largest book in your TBR is{" "}
+                <strong>{bookWithMostPages?.title}</strong> with{" "}
+                <strong>{bookWithMostPages?.pages}</strong> pages!
+              </li>
+              <li>
+                📚 The smallest book in your TBR is{" "}
+                <strong>{bookWithLeastPages?.title}</strong> with{" "}
+                <strong>{bookWithLeastPages?.pages}</strong> pages!
+              </li>
+              <li>
+                📚 If you read{" "}
+                <span>
+                  <input
+                    value={numberOfBooksPerMonth}
+                    type="text"
+                    onChange={handleChangeOfReadBooks}
+                    className="border-b border-zinc-500 w-[80px] text-center font-black"
+                  />
+                </span>{" "}
+                pages per month, it would take you{" "}
+                <strong>
+                  {Math.round(totalNumberOfPages / numberOfBooksPerMonth)}{" "}
+                  months
+                </strong>{" "}
+                to finish your TBR!
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </main>
